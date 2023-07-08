15b1c36b5833be8dcbb4e9a7eb470da2cfeba0ed
Paul Zoltán <paulzoltan02@gmail.com>
2023-07-08 14:28:16 +0200

    Add labels to the contact fields

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Inquiry;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +15,9 @@ class ContactFormType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
-      ->add('name')
-      ->add('email')
-      ->add('message', TextareaType::class);
+      ->add('name', TextType::class, ['label' => 'Neved',])
+      ->add('email', EmailType::class, ['label' => 'E-mail címed', 'invalid_message' => 'Custom error message for invalid email address.',])
+      ->add('message', TextareaType::class, ['label' => 'Üzenet szövege',]);
   }
   public function configureOptions(OptionsResolver $resolver)
   {
